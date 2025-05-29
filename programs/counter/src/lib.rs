37aff3626a91f2b41bb8618aed1d2418361a1f24
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-05-29 23:28:23 +0530

    initialize instruction

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -1,16 +1,41 @@
 use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::anchor::{commit, delegate, ephemeral};
+use ephemeral_rollups_sdk::cpi::DelegateConfig;
+use ephemeral_rollups_sdk::ephem::{commit_accounts, commit_and_undelegate_accounts};
 
 declare_id!("7kJooD2pwiJV7eqbiwLgcZEjKzhAtD2n1MgAbsXiCewf");
+
+pub const TEST_PDA_SEED: &[u8] = b"test-pda";
 
 #[program]
 pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("PDA {} count: {}", counter.key(), counter.count);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init_if_needed, 
+        payer = user,
+        space = 8 + 8,
+        seeds = [TEST_PDA_SEED],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Counter{
+    pub count: u64,
+}
